b9eed196203b070305d3913d1ed22fdd5422ea22
ChenShuCheng <1850062762@qq.com>
2019-09-05 17:45:43 +0800

    MasterFile_Item

---
+++ src/main/java/com/pccw/backend/bean/MasterFile_Item/EditBean.java
@@ -0,0 +1,11 @@
+package com.pccw.backend.bean.MasterFile_Item;
+
+import com.pccw.backend.bean.BaseBean;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EditBean extends BaseBean {
+    private Long id;
+}

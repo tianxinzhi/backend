02f7779eccd2ab19da0f0c3b2076b1509b80fd02
wtw1994 <1161912425@qq.com>
2019-09-23 16:21:47 +0800

    wtw0923

--- src/main/java/com/pccw/backend/bean/masterfile_repo/SearchBean.java
+++ src/main/java/com/pccw/backend/bean/masterfile_repo/SearchBean.java
@@ -4,6 +4,8 @@ package com.pccw.backend.bean.masterfile_repo;
 import com.pccw.backend.annotation.PredicateAnnotation;
 import com.pccw.backend.annotation.PredicateType;
 import com.pccw.backend.bean.BaseSearchBean;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,18 +15,23 @@ import lombok.NoArgsConstructor;
 
  @Data
 @NoArgsConstructor
+ @ApiModel(value = "Shop 模块 - SearchBean", description = "")
 public class SearchBean extends BaseSearchBean {
 
-    @PredicateAnnotation(type = PredicateType.LIKE) 
+    @PredicateAnnotation(type = PredicateType.LIKE)
+    @ApiModelProperty(value="门店编码",name="repoCode",example="")
     private String repoCode;
 
     @PredicateAnnotation(type = PredicateType.LIKE)
+    @ApiModelProperty(value="门店名称",name="repoName",example="")
     private String repoName;
 
     @PredicateAnnotation(type = PredicateType.LIKE)
+    @ApiModelProperty(value="门店地址",name="repoAddr",example="")
     private String repoAddr;
 
     @PredicateAnnotation(type = PredicateType.LIKE)
+    @ApiModelProperty(value="areaId",name="areaId",example="")
     private String areaId;
 
     //private String rightDesc;
--- src/main/java/com/pccw/backend/ctrl/MasterFile_RepoCtrl.java
+++ src/main/java/com/pccw/backend/ctrl/MasterFile_RepoCtrl.java
@@ -8,6 +8,8 @@ import com.pccw.backend.bean.masterfile_repo.EditBean;
 import com.pccw.backend.bean.masterfile_repo.SearchBean;
 import com.pccw.backend.entity.DbResRepo;
 import com.pccw.backend.repository.ResRepoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,26 +22,32 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(methods = RequestMethod.POST,origins = "*", allowCredentials = "false")
 @RequestMapping("/masterfile_repo")
+@Api(value="MasterFile_RepoCtrl",tags={"masterfile_repo"})
 public class MasterFile_RepoCtrl extends BaseCtrl<DbResRepo> {
 
     @Autowired
     ResRepoRepository repo;
 
+    @ApiOperation(value="查询shop",tags={"masterfile_repo"},notes="说明")
     @RequestMapping(method = RequestMethod.POST,path="/search")
     public JsonResult search(@RequestBody SearchBean b) {
         log.info(b.toString());
         return this.search(repo,  b);
     }
 
+    @ApiOperation(value="删除shop",tags={"masterfile_repo"},notes="说明")
     @RequestMapping(method = RequestMethod.POST,path = "/delete")
     public JsonResult delete(@RequestBody BaseDeleteBean ids){
         return this.delete(repo,ids);
     }
 
+    @ApiOperation(value="创建shop",tags={"masterfile_repo"},notes="说明")
     @RequestMapping(method = RequestMethod.POST,path="/create")
     public JsonResult create(@RequestBody CreateBean b){
         return this.create(repo, DbResRepo.class, b);
     }
+
+    @ApiOperation(value="编辑shop",tags={"masterfile_repo"},notes="说明")
     @RequestMapping(method = RequestMethod.POST,path="/edit")
     public JsonResult edit(@RequestBody EditBean b){
         log.info(b.toString());

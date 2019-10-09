ff194beddd4883d2ec95b4602de3553e13a1325e
liujie <601499896@qq.com>
2019-10-09 14:38:03 +0800

    update

--- src/main/java/com/pccw/backend/entity/DbResLogROR.java
+++ src/main/java/com/pccw/backend/entity/DbResLogROR.java
@@ -10,24 +10,24 @@ import javax.persistence.*;
 @Data
 @Entity
 @Table(name = "res_log_ror")
-public class DbResLogRor extends BaseLog {
-	
+public class DbResLogRoR extends BaseLog {
 
-	// POS / BOM / BES
-	@Column(name = "log_sys",length = 8)
-	private String logSys;
 
-	// OrderId 
-	@Column(name="log_orderId",length = 512)
-	private String logOrderId;
-	
-	// Related OrderId
-	@Column(name="log_relatedOrderId",length = 512)
-	private String logRelatedOrderId;
+    // POS / BOM / BES
+    @Column(name = "log_sys",length = 8)
+    private String logSys;
 
-	// N(normal) / A(Advance Order)
-	@Column(name="log_orderType",length = 1)
-	private String logOrderType;
+    // OrderId
+    @Column(name="log_orderId",length = 512)
+    private String logOrderId;
+
+    // Related OrderId
+    @Column(name="log_relatedOrderId",length = 512)
+    private String logRelatedOrderId;
+
+    // N(normal) / A(Advance Order)
+    @Column(name="log_orderType",length = 1)
+    private String logOrderType;
 
 
 

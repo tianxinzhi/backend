26266901ff4933b9e3f270278c1d2f29aeea9d63
wtw1994 <1161912425@qq.com>
2019-10-11 14:43:46 +0800

    stockout

---
+++ src/main/java/com/pccw/backend/ctrl/Stock_OutCtrl.java
@@ -0,0 +1,44 @@
+package com.pccw.backend.ctrl;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/stock_out")
+@CrossOrigin(methods = RequestMethod.POST,origins = "*", allowCredentials = "false")
+public class Stock_OutCtrl {
+
+	// @Autowired
+	// private ResSkuRepoRepository repo;
+
+
+	// /**
+	//  * 
+	//  * @param sc
+	//  * @return
+	//  * @throws IllegalArgumentException
+	//  * @throws IllegalAccessException
+	//  */
+	// @RequestMapping(method=RequestMethod.POST,path="/search")
+	// public JsonResult<DbResSkuRepo> test(@RequestBody SearchCondition sc)
+	//  {
+	//     try {
+	// 		Specification<DbResSkuRepo> spec = Convertor.<DbResSkuRepo,SearchCondition>convertSpecification(SearchCondition.class,sc);
+	// 		List<DbResSkuRepo> res =repo.findAll(spec,PageRequest.of(sc.getPageIndex(),sc.getPageSize())).getContent();
+
+	// 		// List<DbResRepo> r = repoTest.findAll();
+	// 		// log.info(r.toString());
+
+	// 		return JsonResult.success(res);
+	// 	} catch (Exception e) {
+	// 		// log.error(e, t);
+	// 		return JsonResult.fail();
+	// 	}
+	// }
+}

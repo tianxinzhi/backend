e2ef1b4d43254c94e2608eaf36a09b9e31c64824
ChenShuCheng <1850062762@qq.com>
2019-12-05 20:02:26 +0800

    修改返回页面数据结构

--- src/main/java/com/pccw/backend/ctrl/Process_ProcessCtrl.java
+++ src/main/java/com/pccw/backend/ctrl/Process_ProcessCtrl.java
@@ -1,12 +1,18 @@
 package com.pccw.backend.ctrl;
 
 import com.pccw.backend.bean.JsonResult;
-import com.pccw.backend.bean.process_process.ReqOrPedSearchBean;
-import com.pccw.backend.bean.process_process.SearchBean;
+import com.pccw.backend.bean.StaticVariable;
+import com.pccw.backend.bean.process_process.*;
+import com.pccw.backend.entity.DbResLogMgt;
 import com.pccw.backend.entity.DbResProcess;
+import com.pccw.backend.entity.DbResProcessDtl;
+import com.pccw.backend.entity.DbResRole;
+import com.pccw.backend.repository.ResLogMgtRepository;
 import com.pccw.backend.repository.ResProcessRepository;
+import com.pccw.backend.repository.ResRoleRepository;
 import com.pccw.backend.util.Convertor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
@@ -15,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -25,12 +33,18 @@ public class Process_ProcessCtrl extends BaseCtrl{
     @Autowired
     ResProcessRepository processRepository;
 
+    @Autowired
+    ResRoleRepository roleRepository;
+
+    @Autowired
+    ResLogMgtRepository logMgtRepository;
+
     @RequestMapping(method = RequestMethod.POST,path = "/search")
     public JsonResult search(@RequestBody SearchBean bean){
         try {
             List<DbResProcess> res = getDbResProcesses(bean);
-
-            return JsonResult.success(res);
+            List<RecodeBean> list = getRecodes(res);
+            return JsonResult.success(list);
         } catch (Exception e) {
             return JsonResult.fail(e);
         }
@@ -40,7 +54,7 @@ public class Process_ProcessCtrl extends BaseCtrl{
     public JsonResult myReqSearch(@RequestBody ReqOrPedSearchBean bean){
         try {
             List<DbResProcess> res = getDbResProcesses(bean);
-
+            List<RecodeBean> list = getRecodes(res);
             return JsonResult.success(res);
         } catch (Exception e) {
             return JsonResult.fail(e);
@@ -69,5 +83,47 @@ public class Process_ProcessCtrl extends BaseCtrl{
         log.info(bean.toString());
 
         return processRepository.findAll(Convertor.convertSpecification(bean));
+    }
+
+    private String getStepActive(String str) {
+        String status = "";
+
+        switch (str){
+            case StaticVariable.PROCESS_PENDING_STATUS:
+                status = "process";
+                break;
+            case StaticVariable.PROCESS_APPROVED_STATUS:
+                status = "finish";
+                break;
+            case StaticVariable.PROCESS_WAITING_STATUS:
+                status = "wait";
+                break;
+            case StaticVariable.PROCESS_REJECTED_STATUS:
+                status = "error";
+                break;
+        }
+
+        return status;
+    }
+
+    private List<RecodeBean> getRecodes(List<DbResProcess> res) {
+        return res.stream().map(r -> {
+
+            List<DbResLogMgt> logDtls = logMgtRepository.findDbResLogMgtByLogTxtBum(r.getLogTxtBum());
+
+            List<DtlRecodeBean> processDtls = r.getProcessDtls().stream().map(item -> {
+                String roleName = roleRepository.findById(item.getRoleId()).get().getRoleName();
+                Step step = new Step(item.getStatus(), roleName, getStepActive(item.getStatus()));
+                DtlRecodeBean dtlRecodeBean = new DtlRecodeBean(roleName, step);
+                BeanUtils.copyProperties(item, dtlRecodeBean);
+                return dtlRecodeBean;
+            }).collect(Collectors.toList());
+
+            List<Step> steps = processDtls.stream().map(item -> {
+                return item.getStep();
+            }).collect(Collectors.toList());
+
+            return new RecodeBean(processDtls, logDtls, steps);
+        }).collect(Collectors.toList());
     }
 }

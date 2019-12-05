b5ee0d70287412f341f3a8aba3859ac535f90a58
ChenShuCheng <1850062762@qq.com>
2019-12-05 21:11:36 +0800

    update

--- src/main/java/com/pccw/backend/repository/ResLogMgtRepository.java
+++ src/main/java/com/pccw/backend/repository/ResLogMgtRepository.java
@@ -3,10 +3,8 @@ package com.pccw.backend.repository;
 
 import com.pccw.backend.entity.DbResLogMgt;
 
-import java.util.List;
-
 
 public interface ResLogMgtRepository extends BaseRepository<DbResLogMgt> {
 
-    List<DbResLogMgt> findDbResLogMgtByLogTxtBum(String logTxtBum);
+    DbResLogMgt findDbResLogMgtByLogTxtBum(String logTxtBum);
 }

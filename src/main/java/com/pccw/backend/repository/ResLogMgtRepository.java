c611a7617d1e3636418505ee3b6e16a0f9b83088
liujie <601499896@qq.com>
2019-12-24 11:39:41 +0800

    update

--- src/main/java/com/pccw/backend/repository/ResLogMgtRepository.java
+++ src/main/java/com/pccw/backend/repository/ResLogMgtRepository.java
@@ -19,23 +19,24 @@ public interface ResLogMgtRepository extends BaseRepository<DbResLogMgt> {
             "\trlmd.id \"id\",\n" +
             "\trlmd.DTL_REPO_ID \"repoId\",\n" +
             "\trr.REPO_CODE \"repoCode\",\n" +
-            "\trlmd.DTL_SKU_ID \"skuId\",\n" +
-            "\trs.SKU_NAME \"skuName\", \n" +
+            "\trlmd.DTL_SKU_ID \"skuId\" ,\n" +
+            "\trs.SKU_NAME \"skuName\",\n" +
             "\trlmd.DTL_QTY \"qty\",\n" +
             "\trlmd.ACTIVE \"active\",\n" +
             "\trlmd.CREATE_AT \"createAt\",\n" +
-            "\trlmd.CREATE_BY \"createBy\",\n" +
+            "\t(SELECT ACCOUNT_NAME from RES_ACCOUNT where ID = rlmd.CREATE_BY) \"createAccountName\",\n" +
             "\trlmd.UPDATE_AT \"updateAt\",\n" +
-            "\trlmd.UPDATE_BY \"updateBy\"\n" +
+            "\t(SELECT ACCOUNT_NAME from RES_ACCOUNT where ID = rlmd.UPDATE_BY) \"updateAccountName\" \n" +
             "FROM\n" +
             "\tRES_LOG_MGT_DTL rlmd\n" +
-            "\tLEFT JOIN RES_LOG_MGT rlm ON rlmd.LOG_MGT_ID = rlm.ID \n" +
-            "\tLEFT JOIN RES_REPO rr on rr.id = rlmd.DTL_REPO_ID\n" +
-            "\tLEFT JOIN RES_SKU rs on rs.id = rlmd.DTL_SKU_ID\n" +
+            "\tLEFT JOIN RES_LOG_MGT rlm ON rlmd.LOG_MGT_ID = rlm.ID\n" +
+            "\tLEFT JOIN RES_REPO rr ON rr.id = rlmd.DTL_REPO_ID\n" +
+            "\tLEFT JOIN RES_SKU rs ON rs.id = rlmd.DTL_SKU_ID \n" +
             "WHERE\n" +
-            "\trlm.LOG_ORDER_NATURE = 'STHR'\n" +
+            "\trlm.LOG_ORDER_NATURE = 'STHR' \n" +
             "\tand rlmd.DTL_REPO_ID = nvl(?1,rlmd.DTL_REPO_ID)\n" +
             "\tand rlmd.DTL_SKU_ID = nvl(?2,rlmd.DTL_SKU_ID)\n" +
-            "\tORDER BY rlmd.CREATE_AT DESC",nativeQuery = true)
+            "ORDER BY\n" +
+            "\trlmd.CREATE_AT DESC",nativeQuery = true)
     List<Map> getStockThreshold(@Param("repoId") String repoId, @Param("skuId") String skuId);
 }

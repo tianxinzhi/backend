ede80b0ff29eb63b5747025ae63f386bb22fdd65
ChenShuCheng <1850062762@qq.com>
2019-12-25 11:49:54 +0800

    增加人员组织关系

--- src/main/java/com/pccw/backend/repository/ResStockInRepository.java
+++ src/main/java/com/pccw/backend/repository/ResStockInRepository.java
@@ -25,7 +25,7 @@ public interface ResStockInRepository extends BaseRepository<DbResLogMgt>{
             "and rlm.LOG_TXT_NUM = ?1",nativeQuery = true)
     List<Map> getStockOutInfo(String logTxtNum);
 
-    List<DbResLogMgt> findAllByLogOrderNatureInAndLogRepoInEquals(List<String> nature,Long repoId);
+    List<DbResLogMgt> findAllByLogOrderNatureInAndLogRepoInIn(List<String> nature,List repoIds);
 
     DbResLogMgt findDbResLogMgtByLogTxtBum(String logTxtBum);
 }

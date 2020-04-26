763d98df47ccb9f6265c6302aa9b79d2e693b66e
liujie <601499896@qq.com>
2020-04-26 12:56:54 +0800

    update

--- src/main/java/com/pccw/backend/ctrl/Stock_MovementCtrl.java
+++ src/main/java/com/pccw/backend/ctrl/Stock_MovementCtrl.java
@@ -7,6 +7,7 @@ import com.pccw.backend.bean.StaticVariable;
 import com.pccw.backend.bean.stock_movement.SearchBean;
 import com.pccw.backend.entity.*;
 import com.pccw.backend.repository.*;
+import com.pccw.backend.util.CollectionBuilder;
 import com.pccw.backend.util.Convertor;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -70,96 +71,124 @@ public class Stock_MovementCtrl extends BaseCtrl<DbResProcess> {
             Specification spec = Convertor.convertSpecification(b);
             Sort sort = new Sort(Sort.Direction.DESC,"id");
             ArrayList<Map> list = new ArrayList<>();
-            List<DbResProcess> res = processRepo.findAll(spec, PageRequest.of(b.getPageIndex(),b.getPageSize(),sort)).getContent();
-            if(!Objects.isNull(res) && res.size() > 0){
-            for(DbResProcess p:res){
-                Map map = JSON.parseObject(JSON.toJSONString(p), Map.class);
-                map.put("createAccountName",getAccountName(p.getCreateBy()));
-                //movemenet根据不同的nature显示不同的详情
-                if(StaticVariable.LOGORDERNATURE_REPLENISHMENT_REQUEST.equals(p.getLogOrderNature())){
-                    DbResLogRepl dbResLogRepl = logReplRepo.findDbResLogReplByLogTxtBum(p.getLogTxtBum());
-                    String fromName = repoRepo.findById(dbResLogRepl.getRepoIdFrom()).get().getRepoName();
-                    String toName = repoRepo.findById(dbResLogRepl.getRepoIdTo()).get().getRepoName();
-                    map.put("repoName","From: "+fromName+" , To: "+toName);
-                    List<DbResLogReplDtl> line = dbResLogRepl.getLine();
-                    ArrayList<Object> skuQtyList = new ArrayList<>();
-                    for(DbResLogReplDtl dtl:line){
-                        StringBuilder sb = new StringBuilder();
-                        String skuName = skuRepo.findById(dtl.getDtlSkuId()).get().getSkuName();
-                        String skuQtyString = sb.append("SkuName: ").append(skuName).append(" , Qty: ").append(dtl.getDtlQty()).toString();
-                        skuQtyList.add(skuQtyString);
-                    }
-                    map.put("sku",skuQtyList);
-                }else {
-                    DbResLogMgt dbResLogMgt = logMgtRepo.findDbResLogMgtByLogTxtBum(p.getLogTxtBum());
-                    String fromName = "";
-                    if(Objects.nonNull(dbResLogMgt.getLogRepoOut()) && dbResLogMgt.getLogRepoOut() != 0){
-                        fromName = repoRepo.findById(dbResLogMgt.getLogRepoOut()).get().getRepoName();
-                    }
-                    String toName = "";
-                    if(Objects.nonNull(dbResLogMgt.getLogRepoIn()) && dbResLogMgt.getLogRepoIn() != 0){
-                        toName = repoRepo.findById(dbResLogMgt.getLogRepoIn()).get().getRepoName();
-                    }
-                    if(StaticVariable.LOGORDERNATURE_STOCK_THRESHOLD.equals(p.getLogOrderNature())
-                            || StaticVariable.LOGORDERNATURE_STOCK_CATEGORY.equals(p.getLogOrderNature())
-                            || StaticVariable.LOGORDERNATURE_STOCK_TAKE_ADJUSTMENT.equals(p.getLogOrderNature())){
-                        map.put("repoName",fromName);
-                    }else if(StaticVariable.LOGORDERNATURE_STOCK_IN_WITHOUT_PO_STW.equals(p.getLogOrderNature())){
-                        map.put("repoName",toName);
-                    }else {
-                        StringBuilder sb = new StringBuilder();
-                        map.put("repoName",sb.append("From: ").append(fromName).append(" , To: ").append(toName).toString());
-                    }
-                    List<DbResLogMgtDtl> line = dbResLogMgt.getLine();
-                    ArrayList<Object> skuQtyList = new ArrayList<>();
-                    if(!StaticVariable.LOGORDERNATURE_STOCK_CATEGORY.equals(p.getLogOrderNature())){
-                        for(DbResLogMgtDtl dtl:line){
-                            String skuName = skuRepo.findById(dtl.getDtlSkuId()).get().getSkuName();
-                            String skuQtyString = "";
-                            if(StaticVariable.LOGORDERNATURE_STOCK_OUT_STS.equals(p.getLogOrderNature())
-                                    || StaticVariable.LOGORDERNATURE_STOCK_OUT_STW.equals(p.getLogOrderNature())){
-                                if(StaticVariable.DTLSUBIN_GOOD.equals(dtl.getDtlSubin())){
-                                    skuQtyString = "SkuName: "+skuName+" , Qty: "+dtl.getDtlQty();
-                                }
-                            }else {
-                                if(StaticVariable.LOGORDERNATURE_STOCK_TAKE_ADJUSTMENT.equals(p.getLogOrderNature())){
-                                    skuQtyString = "SkuName: "+skuName+" , Qty: "+dtl.getDtlQty()+" , StockType: "+dtl.getDtlSubin()+" , Action: "+dtl.getDtlAction();
-                                }else {
-                                    skuQtyString = "SkuName: " + skuName + " , Qty: " + dtl.getDtlQty();
-                                }
-                            }
-                            if(!"".equals(skuQtyString)){
+
+            //orderAPI
+            if(StaticVariable.LOGORDERNATURE_ASSIGN.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_RETURN.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_EXCHANGE.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_ADVANCED_RESERVE.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_CANCEL_ADVANCE_RESERVE.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_ADVANCE_PICK_UP.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_STOCK_RESERVE.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_STOCK_CANCEL_RESERVE.equals(b.getLogOrderNature()) ||
+                    StaticVariable.LOGORDERNATURE_TRANSFER_TO_WAREHOUSE.equals(b.getLogOrderNature())){
+                Random random = new Random();
+                List arrayList = new ArrayList<>();
+                arrayList.add("SkuName:  iphone-x  , Qty: "+random.nextInt(10));
+                Map m = CollectionBuilder.builder(new HashMap<>())
+                        .put("repoName","shop00"+random.nextInt(3))
+                        .put("sku",arrayList)
+                        .put("active","Y").put("createAccountName","admin")
+                        .put("createAt","1579072220833").put("id",200+random.nextInt(100))
+                        .put("logOrderNature",b.getLogOrderNature())
+                        .put("logTxtBum","200112ISH00309471"+random.nextInt(10))
+                        .put("updateAt","1579072220833").put("updateBy","1")
+                        .build();
+                list.add(m);
+            }else{
+                List<DbResProcess> res = processRepo.findAll(spec, PageRequest.of(b.getPageIndex(),b.getPageSize(),sort)).getContent();
+                if(!Objects.isNull(res) && res.size() > 0){
+                    for(DbResProcess p:res){
+                        Map map = JSON.parseObject(JSON.toJSONString(p), Map.class);
+                        map.put("createAccountName",getAccountName(p.getCreateBy()));
+                        //movemenet根据不同的nature显示不同的详情
+                        if(StaticVariable.LOGORDERNATURE_REPLENISHMENT_REQUEST.equals(p.getLogOrderNature())){
+                            DbResLogRepl dbResLogRepl = logReplRepo.findDbResLogReplByLogTxtBum(p.getLogTxtBum());
+                            String fromName = repoRepo.findById(dbResLogRepl.getRepoIdFrom()).get().getRepoName();
+                            String toName = repoRepo.findById(dbResLogRepl.getRepoIdTo()).get().getRepoName();
+                            map.put("repoName","From: "+fromName+" , To: "+toName);
+                            List<DbResLogReplDtl> line = dbResLogRepl.getLine();
+                            ArrayList<Object> skuQtyList = new ArrayList<>();
+                            for(DbResLogReplDtl dtl:line){
+                                StringBuilder sb = new StringBuilder();
+                                String skuName = skuRepo.findById(dtl.getDtlSkuId()).get().getSkuName();
+                                String skuQtyString = sb.append("SkuName: ").append(skuName).append(" , Qty: ").append(dtl.getDtlQty()).toString();
                                 skuQtyList.add(skuQtyString);
                             }
-                        }
-                    }else{
-                        List itemList = new ArrayList<>();
-                        for (int i = 0; i <line.size(); i=i+2) {
-                            StringBuilder sb = new StringBuilder();
-                            String skuQtyString = "";
-                            String skuName = skuRepo.findById(line.get(i).getDtlSkuId()).get().getSkuName();
-                            skuQtyString = sb.append("SkuName: ").append(skuName).append(" , Qty: ").append(line.get(i).getDtlQty()).append(" , From StockCategory: ").append(line.get(i).getDtlSubin())
-                                    .append(" , To StockCategory: ")
-                                    .append(line.get(i+1).getDtlSubin()).toString();
-                            skuQtyList.add(skuQtyString);
-                            String itemCodes = line.get(i).getItemCode();
-                            String itemCode = "";
-                            if(Objects.nonNull(itemCodes)){
-                                List<String> items = Arrays.asList(itemCodes.split(","));
-                                for (int j = 0;j< items.size();j=j+1){
-                                    itemList.add(skuQtyString+" , itemCode"+(j+1)+": "+items.get(j));
-                                }
+                            map.put("sku",skuQtyList);
+                        }else {
+                            DbResLogMgt dbResLogMgt = logMgtRepo.findDbResLogMgtByLogTxtBum(p.getLogTxtBum());
+                            String fromName = "";
+                            if(Objects.nonNull(dbResLogMgt.getLogRepoOut()) && dbResLogMgt.getLogRepoOut() != 0){
+                                fromName = repoRepo.findById(dbResLogMgt.getLogRepoOut()).get().getRepoName();
                             }
-                        }
+                            String toName = "";
+                            if(Objects.nonNull(dbResLogMgt.getLogRepoIn()) && dbResLogMgt.getLogRepoIn() != 0){
+                                toName = repoRepo.findById(dbResLogMgt.getLogRepoIn()).get().getRepoName();
+                            }
+                            if(StaticVariable.LOGORDERNATURE_STOCK_THRESHOLD.equals(p.getLogOrderNature())
+                                    || StaticVariable.LOGORDERNATURE_STOCK_CATEGORY.equals(p.getLogOrderNature())
+                                    || StaticVariable.LOGORDERNATURE_STOCK_TAKE_ADJUSTMENT.equals(p.getLogOrderNature())){
+                                map.put("repoName",fromName);
+                            }else if(StaticVariable.LOGORDERNATURE_STOCK_IN_WITHOUT_PO_STW.equals(p.getLogOrderNature())){
+                                map.put("repoName",toName);
+                            }else {
+                                StringBuilder sb = new StringBuilder();
+                                map.put("repoName",sb.append("From: ").append(fromName).append(" , To: ").append(toName).toString());
+                            }
+                            List<DbResLogMgtDtl> line = dbResLogMgt.getLine();
+                            ArrayList<Object> skuQtyList = new ArrayList<>();
+                            if(!StaticVariable.LOGORDERNATURE_STOCK_CATEGORY.equals(p.getLogOrderNature())){
+                                for(DbResLogMgtDtl dtl:line){
+                                    String skuName = skuRepo.findById(dtl.getDtlSkuId()).get().getSkuName();
+                                    String skuQtyString = "";
+                                    if(StaticVariable.LOGORDERNATURE_STOCK_OUT_STS.equals(p.getLogOrderNature())
+                                            || StaticVariable.LOGORDERNATURE_STOCK_OUT_STW.equals(p.getLogOrderNature())){
+                                        if(StaticVariable.DTLSUBIN_GOOD.equals(dtl.getDtlSubin())){
+                                            skuQtyString = "SkuName: "+skuName+" , Qty: "+dtl.getDtlQty();
+                                        }
+                                    }else {
+                                        if(StaticVariable.LOGORDERNATURE_STOCK_TAKE_ADJUSTMENT.equals(p.getLogOrderNature())){
+                                            skuQtyString = "SkuName: "+skuName+" , Qty: "+dtl.getDtlQty()+" , StockType: "+dtl.getDtlSubin()+" , Action: "+dtl.getDtlAction();
+                                        }else {
+                                            skuQtyString = "SkuName: " + skuName + " , Qty: " + dtl.getDtlQty();
+                                        }
+                                    }
+                                    if(!"".equals(skuQtyString)){
+                                        skuQtyList.add(skuQtyString);
+                                    }
+                                }
+                            }else{
+                                List itemList = new ArrayList<>();
+                                for (int i = 0; i <line.size(); i=i+2) {
+                                    StringBuilder sb = new StringBuilder();
+                                    String skuQtyString = "";
+                                    String skuName = skuRepo.findById(line.get(i).getDtlSkuId()).get().getSkuName();
+                                    skuQtyString = sb.append("SkuName: ").append(skuName).append(" , Qty: ").append(line.get(i).getDtlQty()).append(" , From StockCategory: ").append(line.get(i).getDtlSubin())
+                                            .append(" , To StockCategory: ")
+                                            .append(line.get(i+1).getDtlSubin()).toString();
+                                    skuQtyList.add(skuQtyString);
+                                    String itemCodes = line.get(i).getItemCode();
+                                    String itemCode = "";
+                                    if(Objects.nonNull(itemCodes)){
+                                        List<String> items = Arrays.asList(itemCodes.split(","));
+                                        for (int j = 0;j< items.size();j=j+1){
+                                            itemList.add(skuQtyString+" , itemCode"+(j+1)+": "+items.get(j));
+                                        }
+                                    }
+                                }
 //                        if(Objects.nonNull(itemList) && itemList.size()>0){
-                            map.put("itemCode",itemList);
+                                map.put("itemCode",itemList);
 //                        }
-                    }
-                    map.put("sku",skuQtyList);
-                }
+                            }
+                            map.put("sku",skuQtyList);
+                        }
 
-                list.add(map);
-            }};
+                        list.add(map);
+                    }};
+            }
+
+
             return JsonResult.success(list);
         } catch (Exception e) {
             log.info(e.getMessage());
